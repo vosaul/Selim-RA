673d09a9d494e82d50409848abab3e72af648182
Mohsinkh786 <83443792+Mohsinkh786@users.noreply.github.com>
2021-04-30 18:19:00 +0530

    Design

---
+++ index.php
@@ -0,0 +1,207 @@
+<!-- body -->
+<?php include('common/head.php'); ?>
+<?php include('common/header.php'); ?>
+<div class=" homepage-logo-block ">
+ <div class="container h-100">
+  <div class="row h-100">
+   <div class="col-md-12 animated wow zoomIn h-100" data-wow-delay="1s">
+    <div class="homepage-logo-img d-flex align-items-center justify-content-center h-100">
+     <img src="images/RAS transparent.svg">
+   </div>
+   <a href="#about_blog" id="scroll-btm" class="">
+     <span>Scroll or click to navigate <img src="images/downarrow.svg"></span>
+   </a>
+ </div>
+
+</div>
+</div>
+</div>
+<div class="support_box">
+ <div class="container">
+  <div class="row">
+   <div class="col-md-6 col-sm-12">
+    <div class="detail-box">
+     <h2 class="aboutHead animated wow fadeIn" data-wow-delay="1s"> Real Assets Support  </h2>
+     <h4 class=" animated wow fadeIn" data-wow-delay="0.5s">Deutschlands größter Makler Betreuer im Edelmetallbereich</h4>
+     <p>Der deutsche Goldmarkt ist aufgrund der geringen gesetzlichen Regelungen und Sicherungen eher turbulent einzuordnen. Auf viele seriöse Anbieter kommen genauso viele eher fragwürdige Anbieter die die gesetzlichen Lücken für sich nutzen. Dazu kommt das im Goldbereich keine so hohen Margen wie z.B. im Finanzbereich möglich sind.
+     </p><p>„Also wie und wo kann ich meine Kunden mit Edelmetall versorgen?“
+      Genau hier kommt der Real Assets Support ins Spiel. Als unabhängiger Maklerbetreuer, arbeiten wir ausschließlich mit Finanz und Versicherungsprofis und haben uns auf Edelmetalle und Ihre Anbieter spezialisiert. 
+    </p>
+
+
+
+
+
+  </div>
+</div>
+<div class="col-md-6 col-sm-12">
+
+  <div class="rgt_img_sctn"><div class="photo_border"></div>
+  <img src="images/supprt_rgt.svg"> 
+</div>  
+</div>
+</div>
+</div>
+</div>
+
+<div class="third_box">
+ <div class="container">
+   <div class="row">
+    <div class=" col-lg-5 col-md-6 col-sm-12 third_box_col">
+     <div class="wdt_box"> 
+      <div class="wrap_box">
+       <p>   Seit 7 Jahren vergleichen und prüfen wir Anbieter, Gesellschaften und Vertriebe auf Herz und Nieren. Mit dieser geballten Edelmetall Expertise wissen wir ganz genau wo wir hinschauen müssen.</p>
+     </div>
+   </div>
+ </div>
+ <div class="offset-lg-2 col-lg-5 col-md-6 col-sm-12 third_box_col">
+  <div class="wdt_box"> 
+   <div class="wrap_box">
+    <p> Wir prüfen von den Kostenstrukturen und vertrieblichen Vernetzung bis hin zur Sicherheit und Kundenfreundlichkeit. Und decken damit jede noch so versteckte Kostenfalle, jede Undurchsichtigkeit in Kundenverträgen und jede Sicherheitslücke auf.</p>
+  </div> 
+</div>
+</div>
+</div>
+
+</div>
+<div class="container-fluid gld_curncy_img">
+ <img src="images/gold_curncy.svg">
+</div>
+
+
+
+
+
+
+<div class="container service_box">
+  <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12">
+     <div class="serv_head">
+      <p> Wir sind für Sie da.</p> 
+      <p> Egal ob Sie in Ihrer Maklertätigkeit erfahrener Edelmetallprofi oder Neueinsteiger,</p>
+      <p> wir unterstützen Sie genauso wie Sie es brauchen.
+      </p>
+    </div>
+    <ul class="align-items-center justify-content-center d-flex">
+      <li>
+       <div>
+        <img src="images/video.svg">
+        <p>Webinare</p>
+      </div>
+    </li>
+    <li>
+     <div>
+      <img src="images/customer.svg">
+      <p>Kundenmaterial</p>
+    </div>
+  </li>
+  <li>
+   <div>
+    <img src="images/chat.svg">
+    <p>Gesprächsvarianten</p>
+  </div>
+</li>
+<li>
+ <div>
+  <img src="images/videos.svg">
+  <p>Erklär Videos</p>
+</div>
+</li>
+<li>
+ <div>
+  <img src="images/bullhorn.svg">
+  <p>Sozialmedia</p>
+</div>
+</li>
+<li>
+ <div>
+   <img src="images/web-search-engine.svg">
+   <p>Website Einbau</p>
+ </div>
+</li>
+<li>
+ <div>
+  <img src="images/advise.svg">
+  <p>Kundenberatung</p>
+</div>
+</li>
+</ul>
+<h1 class="rotated"> Service </h1>
+</div>
+</div>
+</div>
+
+<div class="container-fluid detl_img p-0">
+ <img src="images/details.png">
+</div>
+
+
+</div>
+
+<div class="">   
+  <div class="row m-0">
+    <div class="col-md-6 header_wp_page1">
+     <div class="align-items-center justify-content-start d-flex h-100 container">
+      <div class="rea_head1 text-left">
+        <h1>Produktgeber und Varianten </h1>        
+        <p>Wir checken 24 Stunden täglich jeden Anbieter. Für ein offenes, ehrliches und transparentes Goldprodukt in Ihrem Kundenportfolio. Real Assets Support ist KEIN Edelmetallanbieter!!! Sondern eine unabhänige Gesellschaft die Unternehmen und Anbieter speziell </p>
+
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6 banner-right">
+  </div>
+</div>
+</div>
+<div class="sup_box">
+  <div class="container h-100">   
+    <div class="row h-100">
+      <div class="col-md-6 align-items-center justify-content-start d-flex h-100">
+        <div class="lft_img_box">
+          <img src="images/sprt_gld_cn.svg">
+        </div>
+      </div>
+      <div class="col-md-6 h-100">
+        <div class="align-items-center justify-content-start d-flex h-100 container">
+          <div class="rea_head1 text-left">
+            <h1>Produktgeber und Varianten </h1>        
+            <p>Wir checken 24 Stunden täglich jeden Anbieter. Für ein offenes, ehrliches und transparentes Goldprodukt in Ihrem Kundenportfolio. Real Assets Support ist KEIN Edelmetallanbieter!!! Sondern eine unabhänige Gesellschaft die Unternehmen und Anbieter speziell </p>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="crwn_box">
+  <div class="container h-100">   
+    <div class="row h-100 align-items-center justify-content-start">
+      <div class="col-md-4 ">
+
+        <div class="rea_head1 text-left">       
+          <p>Die beständigste Währung der Welt zählt bis heute als Fels in der Brandung des internationalen Finanzmarktes. Als Absicherung gegen Banken und Finanzkrisen, gegen Pleiten von Gesellschaften und Staaten und vor dem Werterhalts des Vermögens vor Inflation is</p>
+
+        </div>
+
+      </div>
+      <div class="col-md-4 ">
+        <div class="">
+          <img src="images/crwn.svg">
+        </div>
+      </div>
+      <div class="col-md-4">
+
+        <div class="rea_head1 text-left">       
+          <p>Die beständigste Währung der Welt zählt bis heute als Fels in der Brandung des internationalen Finanzmarktes. Als Absicherung gegen Banken und Finanzkrisen, gegen Pleiten von Gesellschaften und Staaten und vor dem Werterhalts des Vermögens vor Inflation is</p>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
+<?php //include('common/footer.php'); ?>
+<?php include('common/footer-script.php'); ?>
